74d7dfea396a7908b92a1ad28dec2880c10ce708
DineshBS44 <dineshbs444@gmail.com>
2020-01-29 12:26:29 +0530

    My first commit

--- app/src/main/java/com/acmvit/androidchallenge1/Activities/DineshActivity.java
+++ app/src/main/java/com/acmvit/androidchallenge1/Activities/DineshActivity.java
@@ -3,6 +3,9 @@ package com.acmvit.androidchallenge1.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.acmvit.androidchallenge1.R;
 
@@ -12,5 +15,15 @@ public class DineshActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dinesh);
+
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"My first commit",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
