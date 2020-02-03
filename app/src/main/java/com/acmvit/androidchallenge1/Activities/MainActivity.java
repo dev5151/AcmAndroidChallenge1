cc93805e3d782abc3bc47a9973f2797200006dbf
akri16 <amit.ananthkumar@gmail.com>
2020-02-03 12:33:54 +0530

    Challenge completed as Favourite Image app

--- app/src/main/java/com/acmvit/androidchallenge1/Activities/MainActivity.java
+++ app/src/main/java/com/acmvit/androidchallenge1/Activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.acmvit.androidchallenge1.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.acmvit.androidchallenge1.R;
 
@@ -12,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btn_amit_krishna);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AmitKrishnaActivity.class);
+                startActivity(intent);
+            }
+        });
 
         /* Steps Needed to be followed:
 
---
+++ app/src/main/java/com/acmvit/androidchallenge1/Models/Item.java
@@ -0,0 +1,29 @@
+package com.acmvit.androidchallenge1.Models;
+
+public class Item {
+    private String url, title, disp;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDisp() {
+        return disp;
+    }
+
+    public void setDisp(String disp) {
+        this.disp = disp;
+    }
+}
